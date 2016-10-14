7babd2f0c10849a44a16e4dab96e8d1879e9efa0
Alma Maizels <aymaiz@yahoo.com>
2016-10-13 22:59:40 -0700

    Helper methods for child classes partly created. Routes methods started but not finished.

--- app/helpers/media_listings_helper.rb
+++ app/helpers/media_listings_helper.rb
@@ -1,2 +1,38 @@
 module MediaListingsHelper
+
+  def created_per_type(listing)
+    if listing.type == "Movie"
+      return "Directed"
+    elsif listing.type == "Book"
+      return "Written"
+    elsif listing.type == "Album"
+      return "Performed"
+    else
+      return "(Unknown media type) Created"
+    end
+  end
+
+  def edit_route(listing)
+    if listing.type == "Movie"
+      return edit_movie_path
+    elsif listing.type == "Book"
+      return edit_book_path
+    elsif listing.type == "Album"
+      return edit_album_path
+    else
+      return Error
+    end
+  end
+
+  def media_listing_route(listing)
+    if listing.type == "Movie"
+      return movie_path
+    elsif listing.type == "Book"
+      return book_path
+    elsif listing.type == "Album"
+      return album_path
+    else
+      return Error
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :books, controller: 'media_listings', type: "Book"
   resources :albums, controller: 'media_listings', type: "Album"
 
-  patch '/media_listings/upvote' => 'media_listings#upvote', as: 'upvote_listing'
+  # patch '/media_listings/upvote' => 'media_listings#upvote', as: 'upvote_listing' #suggested by Chris
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
